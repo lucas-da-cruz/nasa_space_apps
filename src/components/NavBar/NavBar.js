83a6ea129eaa5e575cd8ef245543364152908bf5
Lucas da Cruz <lucas.cruz2000@hotmail.com>
2019-10-20 02:18:12 -0300

    versao 0.2

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,19 +8,33 @@ class Navbare extends Component{
  render(){
     return(
       <div>
-          <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Menu</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link><Link to={"/"}>Home</Link></Nav.Link>
-              <Nav.Link><Link to={"/insert/user"}>Cadastro</Link></Nav.Link>
-              <Nav.Link><Link to={"/user/list"}>Listagem usuários</Link></Nav.Link>
-              <Nav.Link><Link to={"/sensor/list"}>Listagem de incidentes</Link></Nav.Link>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Pesquisar" className="mr-sm-2" />
-              <Button variant="outline-info">Pesquisar</Button>
-            </Form>
-          </Navbar>                   
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+
+        
+        <Nav.Link className="navbar-brand"><Link to={"/"}>Home</Link></Nav.Link>
+
+        <button className="navbar-toggler" data-toggle="collapse" 
+        data-target="#navegacao">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navegacao">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <Nav.Link><Link to={"/create/denuncia"}>Denúncia</Link></Nav.Link>
+                </li>
+                <li className="nav-item">
+                    <Nav.Link><Link to={"/insert/user"}>Cadastro</Link></Nav.Link>
+                </li>
+                <li className="nav-item">
+                    <Nav.Link><Link to={"/user/list"}>Listagem usuários</Link></Nav.Link>
+                </li>
+                <li className="nav-item">
+                    <Nav.Link><Link to={"/sensor/list"}>Listagem de incidentes</Link></Nav.Link>
+                </li>
+            </ul>
+        </div>
+    </nav>            
         </div>
     );
   }
